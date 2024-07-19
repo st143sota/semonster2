c679512d79d8cd80f19151ea6d54ec6b82cf1115
st143sota <0tf7532y328090j@au.com>
2024-07-19 15:27:29 +0900

    push2

---
+++ app/src/main/java/org/example/Battle.java
@@ -0,0 +1,25 @@
+package main.java.org.example;
+
+public class Battle {
+  public static void startBattle(Player p1, Player p2) {
+    for (int i = 0; i < p1.list.size(); i++) {
+      System.out.println("第" + (i + 1) + "回戦...");
+
+      // 対戦するモンスター名とレアリティの表示
+      System.out.println(p1.list.get(i).getMonster() + "[" + p1.list.get(i).getRarity() + "]" + " VS "
+          + p2.list.get(i).getMonster() + "[" + p2.list.get(i).getRarity() + "]");
+
+      // 結果発表
+      if (p1.list.get(i).getRarity() > p2.list.get(i).getRarity()) {
+        System.out.println(p1.name + "の勝利\n");
+      } else if (p1.list.get(i).getRarity() < p2.list.get(i).getRarity()) {
+        System.out.println(p2.name + "の勝利\n");
+      } else {
+        System.out.println("引き分け\n");
+      }
+
+      System.out.println();
+    }
+    return;
+  }
+}
